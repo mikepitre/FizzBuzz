3e2ce9c0a0fb4270d63d38dcf1b685c42b3749b5
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-18 14:09:11 -0500

    normal mode

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 class Fizzbuzz
 
 
-  def run_normal(highest_number)
+  def run(highest_number)
     (1..highest_number).each do |number|
       if number % 3 == 0 && number % 5 == 0
         puts "fizzbuzz"
